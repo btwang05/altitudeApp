14d5b399039dc4c4f78d6a8edf21faea8d79327c
btwang05 <btwang05@gmail.com>
2015-10-15 20:23:59 -0400

    Added Xcode project

---
+++ Altitude_App/Altitude_App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Altitude_App
+//
+//  Created by Boyu Wang on 10/15/15.
+//  Copyright (c) 2015 Boyu Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
