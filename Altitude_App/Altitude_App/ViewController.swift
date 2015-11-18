b4717fb24ae11f79049fb82b244ba9587b60b4a9
janewanggg <wangjiangfeng@hotmail.com>
2015-11-18 00:28:34 -0500

    Changed UI and added pressure measurement

--- Altitude_App/Altitude_App/ViewController.swift
+++ Altitude_App/Altitude_App/ViewController.swift
@@ -9,17 +9,24 @@
 import UIKit
 import MapKit
 import CoreLocation
+import CoreMotion
 
 class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
     @IBOutlet weak var altitudeLabel: UILabel!
     @IBOutlet weak var lacValLabel: UILabel!
     @IBOutlet weak var longValLabel: UILabel!
+    @IBOutlet weak var pressureLabel: UILabel!
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var stopButton: UIButton!
     
     let locationManager = CLLocationManager()
+    let dataProcessingQueue = NSOperationQueue()
+    let altimeter = CMAltimeter()
+    let lengthFormatter = NSLengthFormatter()
+    var altChange: Double = 0
+    
     var lat = "ConstLat"
     var lon = "ConstLon"
     
@@ -28,8 +35,29 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         locationManager.delegate = self
         locationManager.desiredAccuracy = 20
         mapView.delegate = self
-        //locationManager.requestWhenInUseAuthorization()
-        //locationManager.startUpdatingLocation()
+        
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        lengthFormatter.numberFormatter.usesSignificantDigits = false
+        lengthFormatter.numberFormatter.maximumSignificantDigits = 2
+        lengthFormatter.unitStyle = .Short
+        
+        // Prepare altimeter
+        altimeter.startRelativeAltitudeUpdatesToQueue(dataProcessingQueue) {
+            (data, error) in
+            if error != nil {
+                println("There was an error obtaining altimeter data: \(error)")
+            } else {
+                dispatch_async(dispatch_get_main_queue()) {
+                    //Get a relative altitude when you start moving
+                    //self.altChange += data.relativeAltitude as! Double
+                    //self.altitudeLabel.text = "\(self.lengthFormatter.stringFromMeters(self.altChange))"
+                    var lac = String(format:"%3.3f", data.pressure)
+                    self.pressureLabel.text = lac
+                }
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -39,8 +67,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
     //Start to measure altitude
     @IBAction func measureButton(sender: UIButton) {
         // Need to ask for the right permissions
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.startUpdatingLocation()
+        //locationManager.requestWhenInUseAuthorization()
+        //locationManager.startUpdatingLocation()
     }
     
     @IBAction func buttonPressed(sender: UIButton) {
@@ -89,7 +117,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
             // Re-center the map
             mapView.centerCoordinate = location.coordinate
         }
-        locationManager.stopUpdatingLocation()
+        //locationManager.stopUpdatingLocation()
         
     }
     
